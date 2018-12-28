22acd7657cdefe4473f043cdd6b7f55fee91d429
Daniel Miranda <developermiranda@gmail.com>
2018-12-28 12:02:28 -0200

    Preparando dos Controllers - Atualizado

--- app/controllers/api/v1/series_controller.rb
+++ app/controllers/api/v1/series_controller.rb
@@ -1,16 +1,6 @@
-class Api::V1::SearchesController < ApplicationController
-
- def index
-   check_search_value and (return if performed?)
-   search = PgSearch.multisearch(params[:value]).order("created_at DESC")
-   render json: Api::V1::WatchableSerializer.new(search.map(&:searchable)).serialized_json
+class Api::V1::SeriesController < ApplicationController
+ def show
+  @serie = Serie.find(params[:id])
+  render json: Api::V1::SerieSerializer.new(@serie, include:[:episodes], params: { user: current_user }).serialized_json
  end
-
- private
-
-   def check_search_value
-    if params[:value].present? && params[:value].length < 3
-     render json: { errors: "Parameter :value must have at least 3 characters" }
-    end
-  end
 end
